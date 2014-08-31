7aa36054258d4478df98cdd264f96b4425f768eb
xguai <kinsey@Xi-Jins-MacBook-Pro.local>
2014-08-31 02:49:00 -0700

    first commit codepath ios

---
+++ TipsCalculator/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  TipsCalculator
+//
+//  Created by Xi Jin on 8/30/14.
+//  Copyright (c) 2014 Xi Jin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.15, 0.2, 0.3]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        var billAmount = NSString(string: billField.text).doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+}
+
---
+++ TipsCalculatorTests/TipsCalculatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  TipsCalculatorTests.swift
+//  TipsCalculatorTests
+//
+//  Created by Xi Jin on 8/30/14.
+//  Copyright (c) 2014 Xi Jin. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TipsCalculatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
